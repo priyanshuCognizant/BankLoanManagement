fa71d2a04d464d7a25ee30e067f79d9b34413293
HemadurgaBoddupalli <Hemadurga.Boddupalli@cognizant.com>
2025-12-29 08:46:28 +0530

    Update CustomerRepository.java

---
+++ src/main/java/com/cts/BM/repository/CustomerRepository.java
@@ -0,0 +1,13 @@
+package com.cts.BM.repository;
+
+import com.cts.BM.model.Customer;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CustomerRepository extends JpaRepository<Customer,Integer>{
+
+
+
+}
+
